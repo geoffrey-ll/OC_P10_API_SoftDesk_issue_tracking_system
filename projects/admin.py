91012c30ccc95a5c2811f587e8c63a16dda272d3
Geoffrey Llopis <llopis.geoffrey@laposte.net>
2022-03-31 10:43:51 +0200

    ﻿- Modèles des apps. - Représentation dans l'Admin Django. - Teste de création des objects. - Ajout base de données provisoirement.

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
 
+
+from .models import Comment, Contributor, Issue, Project
+
+
 # Register your models here.
+def shorting_title(obj):
+    return obj.title[:20]
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "author_user", "issue", "shorten_description")
+
+    def shorten_description(self, obj):
+        return obj.description[:50]
+
+
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "project", "role")
+
+
+class IssueAdmin(admin.ModelAdmin):
+    list_display = ("id", "author_user", "assignee_user", "project",
+                    "shorten_title", "tag", "priority", "status")
+
+    def shorten_title(self, obj):  # Rennomer title ?? view_title ??
+        return shorting_title(obj)
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("id", "shorten_title", "type", "author_user")
+
+    def shorten_title(self, obj):  # Rennomer title ?? view_title ??
+        return shorting_title(obj)
+
+
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Contributor, ContributorAdmin)
+admin.site.register(Issue, IssueAdmin)
+admin.site.register(Project, ProjectAdmin)
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,4 @@
+# from django.contrib.auth.models import User
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -6,33 +7,39 @@ from users.models import User
 
 
 # Create your models here.
-class Project:
+class Project(models.Model):
     class ProjectType(models.TextChoices):
         BACK_END = "b", _("Back-end")
         FRONT_END = "f", _("Front-end")
         IOS = 'i', _("iOS")
         ANDROID = 'a', _("Android")
 
-    # project_id = models.IntegerField()   # Nécessaire ?? id déjà par défault
-    title = models.CharField(max_lengt=100)
-    description = models.CharField(max_length=5_000)
-    type = models.CharField(choices=ProjectType.choices,
+    # project_id = models.IntegerField(unique=True)  # À rajouter si besoin.
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    type = models.CharField(max_length=1, choices=ProjectType.choices,
                             default=ProjectType.BACK_END)
-    author_user = models.ForeignKey(to=Contributor, on_delete=models.CASCADE)
+    author_user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+    # Ajouter fonction pour généner automatiquement un contributor avec le
+    # rôle de superviseur, pour le author_user de Project
+
+    class Meta:
+        unique_together = ("id", "author_user")
+
+    def __str__(self):
+        return f"{self.id} : {self.title[:20]}"
 
 
-class Contributor:
+class Contributor(models.Model):
     class ContributorRole(models.TextChoices):
         MANAGER = 'm', _("Superviseur")
         CONTRIBUTOR = 'c', _("Contributeur")
 
-    # IntegerField dans le word…
     user = models.ForeignKey(to=User, on_delete=models.CASCADE)
-    # IntegerField dans le word…
     project = models.ForeignKey(to=Project, on_delete=models.CASCADE)
-    # ChoiceField n'existe pas ……
     # permission = models.ChoiceField()
-    role = models.CharField(choices=ContributorRole.choices,
+    role = models.CharField(max_length=1, choices=ContributorRole.choices,
                             default=ContributorRole.CONTRIBUTOR)
 
     class Meta:
@@ -40,14 +47,14 @@ class Contributor:
         unique_together = ("user", "project")
 
 
-class Issue:
+class Issue(models.Model):
     class IssueTag(models.TextChoices):
         BUG = 'b', _("Bug")
         TASK = 't', _("Tâche")
         ENHANCEMENT = 'e', _("Amélioration")
 
     class IssuePriority(models.TextChoices):
-        WEAK = 'w', _("Faible")
+        LOW = 'l', _("Faible")
         AVERAGE = 'a', _("Moyenne")
         HIGH = 'h', _("Haute")
 
@@ -57,25 +64,27 @@ class Issue:
         COMPLETED = 'c', _("Terminé")
 
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=5_000)
-    tag = models.CharField(choices=IssueTag.choices, default=IssueTag.TASK)
-    priority = models.CharField(choices=IssuePriority,
+    description = models.TextField(null=True, blank=True)
+    tag = models.CharField(max_length=1, choices=IssueTag.choices,
+                           default=IssueTag.TASK)
+    priority = models.CharField(max_length=1, choices=IssuePriority.choices,
                                 default=IssuePriority.AVERAGE)
-    # IntegerField dans le word…
     project = models.ForeignKey(to=Project, on_delete=models.CASCADE)
-    status = models.CharField(choices=IssueStatus, default=IssueStatus.TO_DO)
-    author_user = models.ForeignKey(to=User, on_delete=models.CASCADE)
-    assignee_user = models.ForeignKey(to=User, default=author_user,
+    status = models.CharField(max_length=1, choices=IssueStatus.choices,
+                              default=IssueStatus.TO_DO)
+    author_user = models.ForeignKey(to=User, related_name="created_issues",
+                                    on_delete=models.CASCADE)
+    assignee_user = models.ForeignKey(to=User, related_name="assigned_issues",
+                                      default=author_user,
                                       on_delete=models.CASCADE)
     created_time = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.id} : {self.title[:20]}"
 
-class Comment:
-    # comment_id = models.IntegerField()   # Nécessaire ?? id déjà par défault
-    description = models.CharField(max_lenght=5_000)
+
+class Comment(models.Model):
+    description = models.TextField(null=True, blank=True)
     author_user = models.ForeignKey(to=User, on_delete=models.CASCADE)
     issue = models.ForeignKey(to=Issue, on_delete=models.CASCADE)
     created_time = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        unique_together = ("issue", "author_user")
