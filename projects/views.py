856e0bd4b75b94a916ec9fb82189f77d2574bece
Geoffrey Llopis <llopis.geoffrey@laposte.net>
2022-04-24 12:15:56 +0200

    - projects.views.py : Ajout d'un fonction pour vérifier que l'utilisateur est authentifier et que l'utilisateur est contributeur du project, avant de retourner le queryset des /users, /issues, /issues/{pk}/comments.

--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,5 @@
-from rest_framework.exceptions import NotAcceptable
+from rest_framework.exceptions import NotAcceptable, NotAuthenticated, \
+    PermissionDenied
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet, ViewSet
 
 
@@ -8,11 +9,28 @@ from projects.serializers import ContributorListSerializer, \
 
 
 # Create your views here.
+MESSAGE_NOT_AUTHENTICATED = "Cette API nécessite d'être authentifié."
+MESSAGE_PERMISSION_DENIED = "Accès refusé."
+
+
+def check_authenticated(self):
+    if self.request.user.is_authenticated is False:
+        raise NotAuthenticated(detail=MESSAGE_NOT_AUTHENTICATED)
+    project = Project.objects.get(id=self.kwargs["project_pk"])
+    try:
+        project.contributors.get(user=self.request.user)
+        return project
+    except:
+        raise PermissionDenied(detail=MESSAGE_PERMISSION_DENIED)
+    return
+
+
 class ContributorViewSet(ModelViewSet):
     serializer_class = ContributorListSerializer
 
     def get_queryset(self):
-        return Contributor.objects.filter(project=self.kwargs["project_pk"])
+        project = check_authenticated(self)
+        return Contributor.objects.filter(project=project)
 
     def perform_create(self, serializer):
         if self.request.user.is_authenticated:
@@ -30,6 +48,7 @@ class CommentViewSet(ModelViewSet):
     serializer_class = CommentListSerializer
 
     def get_queryset(self):
+        check_authenticated(self)
         return Comment.objects.filter(issue=self.kwargs["issue_pk"])
 
     def perform_create(self, serializer):
@@ -42,16 +61,18 @@ class IssueViewSet(ModelViewSet):
     serializer_class = IssueListSerializer
 
     def get_queryset(self):
-        return Issue.objects.filter(project=self.kwargs["project_pk"])
+        project = check_authenticated(self)
+        return Issue.objects.filter(project=project)
 
     def perform_create(self, serializer):
         if self.request.user.is_authenticated:
             project = Project.objects.get(id=self.kwargs["project_pk"])
 
+
             contributors_user = [c.user for c in project.contributors.all()]
+            print(f"\nproject_contributors:\n{contributors_user}\n")
             assigned_user = serializer.validated_data['assignee_user']
 
-            # serializer["author_user"] = self.request.user
             if assigned_user not in contributors_user:
                 raise NotAcceptable(
                     "L'utilisateur n'est pas contributeur à ce project."
@@ -66,6 +87,7 @@ class ProjectViewSet(ModelViewSet):
     serializer_class = ProjectListSerializer
 
     def get_queryset(self):
+        check_authenticated(self)
         contributors_user = Contributor.objects.filter(user=self.request.user)
         projects_user = [c.project.id for c in contributors_user]
         return Project.objects.filter(id__in=projects_user)
