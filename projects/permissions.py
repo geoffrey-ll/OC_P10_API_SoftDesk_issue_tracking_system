07f719857356689a8da8df55f2b64658e7d63e9b
Geoffrey Llopis <llopis.geoffrey@laposte.net>
2022-05-24 08:43:40 +0200

    Refonte des permissions terminé.

--- projects/permissions.py
+++ projects/permissions.py
@@ -2,113 +2,19 @@ from rest_framework.exceptions import NotAuthenticated, PermissionDenied
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
-from .messages_error import MESSAGE_PERMISSION_DENIED, MESSAGE_PERMISSION_NOT_AUTHENTICATED
+from .messages_error import (
+    MESSAGE_PERMISSION_DENIED,
+    MESSAGE_PERMISSION_NOT_AUTHENTICATED
+)
 from .models import Contributor, Project
 
-# ANCIEN CODE DES PERMISSIONS
 
-# class ElementAuthorPermission(BasePermission):
-#     def has_permission(self, request, view):
-#         if request.user.is_authenticated:
-#             if request.user.is_superuser:
-#                 return True
-#                 # if request.method not in SAFE_METHODS:
-#                 #     return False
-#             if view.__class__.__name__ == "ContributorViewSet":
-#                 return \
-#                     ProjectAuthorPermission.has_permission(self, request, view)
-#             return True
-#
-#     def has_object_permission(self, request, view, obj):
-#         if request.method in SAFE_METHODS:
-#             return True
-#         try:
-#             return obj.author_user == request.user
-#         except:
-#             pass
-#         try:
-#             if obj.user:
-#                 if request.user.is_superuser:
-#                     if request.method is "PUT":
-#                         return True
-#                     return True
-#                 return ProjectAuthorPermission.has_object_permission(
-#                     self, request, view, obj
-#                 )
-#         except:
-#             pass
-#         return False
-#
-#
-# class ProjectAuthorPermission(BasePermission):
-#     def has_permission(self, request, view):
-#         try:
-#             project = Project.objects.get(id=view.kwargs["project_pk"])
-#         except:
-#             raise PermissionDenied(MESSAGE_PERMISSION_DENIED)
-#         if project.author_user != request.user:
-#             return request.method in SAFE_METHODS
-#         return True
-#
-#     def has_object_permission(self, request, view, obj):
-#         project = Project.objects.get(id=obj.project.id)
-#         return project.author_user == request.user
-#
-#
-# class ProjectContributorPermission(BasePermission):
-#     # def has_permission(self, request, view):
-#     #     non défini. Un problème de sécurité ou d'accès ????
-#     #     À priori n'est appelé que lorsqu'une vue fait un get_queryset,
-#     #     donc uniquement lorsque has_object_permission est appelé
-#
-#     def has_object_permission(self, request, view, obj):
-#         if request.user.is_superuser:
-#             return True
-#         if request.user not in [c.user for c in obj]:
-#             raise PermissionDenied(MESSAGE_PERMISSION_DENIED)
-#
-#
-# class TestPermission(BasePermission):
-#     def has_permission(self, request, view):
-#         return True
-#     def has_object_permission(self, request, view, obj):
-#         return True
-#____________________________________
-
-
-
-# NOUVEAU CODE DES PERMISSIONS
-
-class DefaultPermission(BasePermission):
-    # Échec factorisation des éléments communs aux has_perm et has_obj
-    #
-    # def assign_permission(self, request, view, option):
-    #     if request.user.is_superuser:
-    #         if option == "has_permission":
-    #             return SuperuserPermission().has_permission(request, view)
-    #         else:
-    #             return SuperuserPermission().has_object_permission(
-    #                 request, view, obj
-    #             )
-    #     else:
-    #         # project = Project.objects.get(id=view.kwargs["project_pk"])
-    #         my_role = Contributor.objects.get(
-    #             project=view.kwargs["project_pk"],
-    #             user=request.user
-    #         )
-    #         if my_role == 'm':
-    #             return ManagerPermission()
-    #         if my_role == 'c':
-    #             return ContributorPermission()
-    #_____________________
-
+class AssignPermission(BasePermission):
     def get_my_role(self, request, view):
         if view.__class__.__name__ == "ProjectViewSet":
             project_id = view.kwargs["pk"]
         else:
             project_id = view.kwargs["project_pk"]
-
-        print(f"\n{Contributor.objects.exist(project=project_id, user=request.user)}\n")
 
         return Contributor.objects.get(
             project=project_id, user=request.user
@@ -166,27 +72,24 @@ class DefaultPermission(BasePermission):
 
 class ManagerPermission(BasePermission):
     def has_permission(self, request, view):
-        print(f"\nMANAGER   HAS")
         return True
 
     def has_object_permission(self, request, view, obj):
-        print(f"\nMANAGER   OBJ")
         if view.__class__.__name__ == "ContributorViewSet" \
                 or view.__class__.__name__ == "ProjectViewSet":
             return True
+
         if request.user != obj.author_user:
             return request.method in SAFE_METHODS
 
 
 class ContributorPermission(BasePermission):
     def has_permission(self, request, view):
-        print(f"\nCONTRIBUTEUR   HAS")
         if view.__class__.__name__ is "ContributorViewSet":
             return request.method in SAFE_METHODS
         return True
 
     def has_object_permission(self, request, view, obj):
-        print(f"\nCONTRIBUTEUR   OBJ")
         if view.__class__.__name__ == "ContributorViewSet":
             return request.method in SAFE_METHODS
 
